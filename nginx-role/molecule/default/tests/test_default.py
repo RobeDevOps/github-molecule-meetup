5feb02a6d0c69e5aa9c06e88c8a85288ddcba3e7
roberto <roberto.cardenas.isla@gmail.com>
2020-08-12 16:02:18 -0400

    adding prepare

--- nginx-role/molecule/default/tests/test_default.py
+++ nginx-role/molecule/default/tests/test_default.py
@@ -1,10 +1,14 @@
 """Role testing files using testinfra."""
 
+def test_packages_are_installed(host):
+    required_package = host.package('nginx')
+    assert required_package.is_installed
 
-def test_hosts_file(host):
-    """Validate /etc/hosts file."""
-    f = host.file("/etc/hosts")
+def test_nginx_service_is_enabled_and_running(host):
+    nginx_service = host.service('nginx')
+    assert nginx_service.is_enabled
+    assert nginx_service.is_running
 
-    assert f.exists
-    assert f.user == "root"
-    assert f.group == "root"
+def test_nginx_server_is_available(host):
+    nginx_server = host.addr("localhost")
+    assert nginx_server.port('80').is_reachable
